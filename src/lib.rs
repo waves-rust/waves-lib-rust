a1b1c71ca96ae8e3c310bbc6f87e0543f7ba2c7b
peterz <peterz@rambler.ru>
2018-08-17 16:25:55 +0300

    sign & sig_verify

---
+++ src/lib.rs
@@ -0,0 +1,105 @@
+extern crate base58;
+extern crate curve25519_dalek;
+extern crate ed25519_dalek; // for LENGTH constants
+extern crate rand;
+extern crate sha2;
+
+use curve25519_dalek::constants;
+use curve25519_dalek::montgomery::MontgomeryPoint;
+use curve25519_dalek::scalar::Scalar;
+use ed25519_dalek::*;
+use rand::Rng;
+use sha2::{Digest, Sha512};
+
+pub fn sign(message: &[u8], secret_key: &[u8; SECRET_KEY_LENGTH]) -> [u8; SIGNATURE_LENGTH] {
+    let mlen = message.len();
+    let mut buf: Vec<u8> = vec![0; mlen + 128];
+    let mut rand= rand::thread_rng();
+
+    buf[0] = 0xfe;
+    (1..32).for_each(|i| buf[i] = 0xff);
+    buf[32..64].copy_from_slice(secret_key);
+    buf[64..64 + mlen].copy_from_slice(message);
+    (65 + mlen..128 + mlen).for_each(|i| buf[i] = rand.gen::<u8>());
+
+    let mut hash = Sha512::default();
+    hash.input(&buf);
+    let rsc = Scalar::from_hash(hash);
+    let r = (&rsc * &constants::ED25519_BASEPOINT_TABLE).compress().to_bytes();
+
+    let ed_pubkey = &constants::ED25519_BASEPOINT_POINT * &Scalar::from_bits(*secret_key);
+    let pubkey = ed_pubkey.compress().to_bytes();
+
+    hash = Sha512::default();
+    buf[..32].copy_from_slice(&r);
+    buf[32..64].copy_from_slice(&pubkey);
+    hash.input(&buf[..64 + mlen]);
+    let s = &(&Scalar::from_hash(hash) * &Scalar::from_bits(*secret_key)) + &rsc;
+
+    let sign = pubkey[31] & 0x80;
+    let mut result = [0; SIGNATURE_LENGTH];
+    result[..32].copy_from_slice(&r);
+    result[32..].copy_from_slice(&s.to_bytes());
+    result[63] &= 0x7F; // should be zero already, but just in case
+    result[63] |= sign;
+    result
+}
+
+pub fn sig_verify(message: &[u8], public_key: &[u8; PUBLIC_KEY_LENGTH], signature: &[u8; SIGNATURE_LENGTH]) -> bool {
+    let sign = signature[63] & 0x80;
+    let mut sig = [0u8; SIGNATURE_LENGTH];
+    sig.copy_from_slice(signature);
+    sig[63] &= 0x7f;
+
+    let mut ed_pubkey = MontgomeryPoint(*public_key).to_edwards(sign).unwrap().compress().to_bytes();
+    ed_pubkey[31] &= 0x7F;  // should be zero already, but just in case
+    ed_pubkey[31] |= sign;
+
+    PublicKey::from_bytes(&ed_pubkey).unwrap()
+        .verify::<Sha512>(message,&Signature::from_bytes(&sig).unwrap())
+        .is_ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use base58::*;
+
+    #[test]
+    fn test_signatures() {
+        for _ in 1..50 {
+            let msg: [u8; 32] = rand::thread_rng().gen();
+            let mut sk = [0u8; 32];
+            sk.copy_from_slice(&"25Um7fKYkySZnweUEVAn9RLtxN5xHRd7iqpqYSMNQEeT".from_base58().unwrap().as_slice());
+            let sig = sign( & msg, &sk);
+            println ! ("(\"{}\", \"{}\", \"{}\"),", msg.to_base58(), sk.to_base58(), sig.to_base58());
+        }
+        assert!(true);
+    }
+
+    #[test]
+    fn test_signature() {
+        let msg = "bagira".as_bytes();
+        let mut sk = [0u8; SECRET_KEY_LENGTH];
+        sk.copy_from_slice(&"25Um7fKYkySZnweUEVAn9RLtxN5xHRd7iqpqYSMNQEeT".from_base58().unwrap().as_slice());
+        let mut pk = [0u8; PUBLIC_KEY_LENGTH];
+        pk.copy_from_slice("GqpLEy65XtMzGNrsfj6wXXeffLduEt1HKhBfgJGSFajX".from_base58().unwrap().as_slice());
+        let sig = sign(msg, &sk);
+        println!("sig = {}", sig.to_base58());
+        assert!(sig_verify(msg, &pk, &sig))
+    }
+
+    #[test]
+    fn test_verify() {
+        let msg = "bagira".as_bytes();
+        let mut pk = [0u8; PUBLIC_KEY_LENGTH];
+        pk.copy_from_slice(
+            "GqpLEy65XtMzGNrsfj6wXXeffLduEt1HKhBfgJGSFajX".from_base58().unwrap().as_slice());
+        let mut sig = [0u8; SIGNATURE_LENGTH];
+        sig.copy_from_slice(
+            "62Nc9BbpuJziRuuXvnYttT8hfWXsUPH1kAUfc2fBhLeuCV5szWW7GGFRtqRxbQd92p8cDaHKfUqXdkwcefXSHdp7"
+                .from_base58().unwrap().as_slice());
+
+        assert!(sig_verify(msg, &pk, &sig));
+    }
+}
