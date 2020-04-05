a51d086471ed3fdfaa12fb54316a7041f262365d
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-04-05 14:44:57 +0700

    Added seed.rs

---
+++ src/seed.rs
@@ -0,0 +1,19 @@
+use bip39::{Language, Mnemonic, MnemonicType};
+
+pub fn generate_phrase() -> String {
+    let mnemonic = Mnemonic::new(MnemonicType::Words18, Language::English);
+    let phrase: &str = mnemonic.phrase();
+    format!("{}", phrase)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_phrase() {
+        let phrase = generate_phrase();
+
+        assert_eq!(phrase.split_ascii_whitespace().count(), 18);
+    }
+}
