e1ddf9b7dc5db5929edf09a229a6cbdf5d54c050
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-04-07 19:33:58 +0700

    Added util test

--- src/util.rs
+++ src/util.rs
@@ -25,3 +25,49 @@ pub fn sig_verify(
         .verify(message, &Signature::from_bytes(&sig).unwrap())
         .is_ok()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use base58::*;
+    use crate::account::*;
+
+    #[test]
+    fn test_sig_roundtrip() {
+        let msg = "uncle".as_bytes();
+        let mut sk = [0u8; SECRET_KEY_LENGTH];
+        sk.copy_from_slice(
+            &"6zFSymZAoaua3gtJPbAUwM584tRETdKYdEG9BeEnZaGW"
+                .from_base58()
+                .unwrap()
+                .as_slice(),
+        );
+        let mut pk = [0u8; PUBLIC_KEY_LENGTH];
+        pk.copy_from_slice(
+            "4KxUVD9NtyRJjU3BCvPgJSttoJX7cb3DMdDTNucLN121"
+                .from_base58()
+                .unwrap()
+                .as_slice(),
+        );
+        let sig = sign(msg, &sk);
+        println!("sig = {}", sig.to_base58());
+        assert!(sig_verify(msg, &pk, &sig));
+    }
+
+    #[test]
+    fn test_sig_verify() {
+        let msg = "uncle".as_bytes();
+        let mut pk = [0u8; PUBLIC_KEY_LENGTH];
+        pk.copy_from_slice(
+            "4KxUVD9NtyRJjU3BCvPgJSttoJX7cb3DMdDTNucLN121"
+                .from_base58()
+                .unwrap()
+                .as_slice(),
+        );
+        let mut sig = [0u8; SIGNATURE_LENGTH];
+        sig.copy_from_slice(
+            "B4ViRpS6wZ73hhTtP4hhrfV46rR3uoUn7jgsH5yfkKMpbJUxMmu48jf3QSdibRkQBN7Tkx9jReKDq1Rmp9acxPG"
+                .from_base58().unwrap().as_slice());
+        assert!(sig_verify(msg, &pk, &sig));
+    }
+}
