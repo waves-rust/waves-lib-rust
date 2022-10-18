75bf6b930dc5c209e43de0b054f376c80e3f6787
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2022-10-18 12:03:18 +0700

    Added Debug impl for Hash struct

--- src/transaction/hash.rs
+++ src/transaction/hash.rs
@@ -14,7 +14,7 @@ pub(crate) const HASH_LENGTH: usize = 32;
 /// ```plain_text
 /// YwVPf35VckF4Yu5XwF18P9VwWwfQVGAQmqDp4bpgtuV
 /// ```
-#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Default, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct Hash([u8; HASH_LENGTH]); //// converge w/ Address using macro
 
 impl Hash {
@@ -33,6 +33,12 @@ impl Hash {
         let mut bytes = [0u8; HASH_LENGTH];
         bytes.copy_from_slice(base58.from_base58().unwrap().as_slice()); ////map unwrap, handle bad length
         Hash(bytes)
+    }
+}
+
+impl fmt::Debug for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0.to_base58())
     }
 }
 
